72fcb23d535cca09d655fc1b30819ca34f093315
drillby <podrazkypavel002@gmail.com>
2023-05-03 21:19:24 +0200

    added comments

--- Arduino/libs/WiFiConnController.h
+++ Arduino/libs/WiFiConnController.h
@@ -3,15 +3,27 @@
 #include "Arduino.h"
 #include "WiFiNINA.h"
 
+/// @brief třída řešící připojení k WiFi
 class WiFiConnController
 {
 public:
+    /// @brief třída řešící připojení k WiFi
+    /// @param ssid název WiFi sítě
+    /// @param pw heslo do WiFi sítě
+    /// @param device_ip IP adresa pod kterou se Arduino pokusí přihlásit
+    /// @param encryption druh zabezpečení WiFi sítě: 0 -> bez zabezpečení, 1 -> WEP, 2 -> WPA
     WiFiConnController(char *ssid, char *pw, uint8_t device_ip[], uint8_t encryption);
+    /// @brief pokusí se připojit Arduino k WiFi, musí být zavolána v setup()
+    /// @return None
     void connect();
     char *p_ssid;
     uint8_t ip[4];
     uint8_t status;
+    /// @brief zkoumá zda Arduino má WiFi modul
+    /// @return true pokus Arduino má WiFi modul
     bool hasWiFiModule();
+    /// @brief zkoumá zda Arduino má nejnovější firmware na ovládání WiFi modulu
+    /// @return true pokud má nejnovější firmware
     bool hasLatestFirmware();
 
 private:
