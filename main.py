2bda0d7141dec13549dd44b39781266b2859a5a9
drillby <podrazkypavel002@gmail.com>
2023-05-09 21:03:28 +0200

    added cors

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+api_cors_config = {
+    "origins": ["*"],
+    "methods": ["GET", "POST"],
+    "allow_headers": ["Authorization", "Content-Type", "Access-Control-Allow-Origin"],
+}
+CORS(app, resources={"/*": api_cors_config})
 
 
 @app.route("/", methods=["GET"])
