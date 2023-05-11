3222bdfe8d09475c014b7be9c7711377a1f0715f
drillby <podrazkypavel002@gmail.com>
2023-05-11 21:50:17 +0200

    created lib for communicating with backend API

---
+++ APIController.cpp
@@ -0,0 +1,51 @@
+#include "Arduino.h"
+#include "libs/APIController.h"
+
+APIController::APIController(String server_url, uint8_t server_port)
+{
+    url = server_url;
+    port = server_port;
+    WiFiClient client;
+}
+
+void APIController::connect(uint8_t num_of_tries)
+{
+    for (uint8_t i = 0; i <= num_of_tries; i++)
+    {
+        if (client.connected())
+        {
+            break;
+        }
+        client.connect(url.c_str(), port);
+        delay(10000);
+    }
+}
+
+void APIController::sendRequest(uint8_t req_type, String endpoint, bool keep_alive)
+{
+    if (req_type == 1)
+    {
+        _getRequest(endpoint, keep_alive);
+    }
+    else if (req_type == 2)
+    {
+        _postRequest(endpoint, keep_alive);
+    }
+}
+
+void APIController::_getRequest(String endpoint, bool keep_alive)
+{
+}
+
+void APIController::_postRequest(String endpoint, bool keep_alive)
+{
+}
+
+char APIController::_readRespose()
+{
+    while (!client.available())
+    {
+        ;
+    }
+    return client.read();
+}
--- libs/APIController.h
+++ libs/APIController.h
@@ -1,12 +1,21 @@
 #ifndef APIController_h
 #define APIController_h
+#include "WiFiNINA.h"
 
 class APIController
 {
 public:
-    APIController(String url, uint8_t port);
+    APIController(String server_url, uint8_t server_port);
     void connect(uint8_t num_of_tries);
-    void sendRequest(uint8_t req_type, String endpoint);
+    void sendRequest(uint8_t req_type, String endpoint, bool keep_alive);
+    String url;
+    uint8_t port;
+
+private:
+    void _getRequest(String endpoint, bool keep_alive);
+    void _postRequest(String endpoint, bool keep_alive);
+    char _readRespose();
+    WiFiClient client;
 };
 
 enum RequestType
