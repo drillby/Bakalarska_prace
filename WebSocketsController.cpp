87f9f26c9f313b39da015957f019a1a82446ede0
drillby <podrazkypavel002@gmail.com>
2023-06-05 18:18:05 +0200

    implementeed websockets libs

---
+++ WebSocketsController.cpp
@@ -0,0 +1,43 @@
+#include "Arduino.h"
+#include "libs/WebSocketsController.h"
+
+WebSocketsController::WebSocketsController(String server_url, uint16_t server_port)
+{
+    url = server_url;
+    port = server_port;
+}
+
+WebSocketsController::WebSocketsController(IPAddress server_ip, uint16_t server_port)
+{
+    ip = server_ip;
+    port = server_port;
+}
+
+void WebSocketsController::connect(WiFiClient conn, String endpoint)
+{
+    _conn = conn;
+    if (url != "")
+    {
+        _client = WebSocketClient(_conn, url, port);
+    }
+    else
+    {
+        _client = WebSocketClient(_conn, ip, port);
+    }
+    _client.begin(endpoint);
+}
+
+void WebSocketsController::send(String msg)
+{
+    _client.beginMessage(TYPE_TEXT);
+    _client.print(msg);
+    _client.endMessage();
+}
+
+String WebSocketsController::receive()
+{
+    int isMessage = _client.parseMessage();
+
+    if (isMessage > 0)
+        return _client.readString();
+}
---
+++ libs/WebSocketsController.h
@@ -0,0 +1,24 @@
+#ifndef WebSocketsController_h
+#define WebSocketsController_h
+#include <ArduinoHttpClient.h>
+#include <WiFi.h>
+
+class WebSocketsController
+{
+public:
+    WebSocketsController(String server_url, uint16_t server_port);
+    WebSocketsController(IPAddress server_url, uint16_t server_port);
+    void connect(WiFiClient conn, String endpoint = "");
+    void send(String msg);
+    String receive();
+    String url = "";
+    IPAddress ip;
+    uint16_t port;
+
+private:
+    WiFiClient _conn;
+    WebSocketClient _client = WebSocketClient(_conn, (127, 0, 0, 1), 5000);
+    WebSocketClient _client = WebSocketClient(_conn, "localhost", 5000);
+};
+
+#endif
