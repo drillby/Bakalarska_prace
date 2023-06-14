ee0220beab8b1b88f3faebc981d986d8cea33b67
drillby <podrazkypavel002@gmail.com>
2023-06-14 18:38:07 +0200

    fixed mqtt

--- MQTTController.cpp
+++ MQTTController.cpp
@@ -19,19 +19,13 @@ MQTTController::MQTTController(IPAddress broker, uint16_t port, String sub_topic
 bool MQTTController::connect()
 {
     MqttClient _mqtt = MqttClient(_wifi);
-    WiFi.begin("Zizice_doma", "Pavel_Olinka");
     if (broker_url != "")
     {
-        return !_mqtt.connect(broker_url.c_str(), broker_port);
+        return _mqtt.connect(broker_url.c_str(), broker_port);
     }
     else
-        return !_mqtt.connect(broker_ip, broker_port);
+        return _mqtt.connect(broker_ip, broker_port);
 }
-
-// bool MQTTController::connect()
-// {
-//     _mqtt.connect(broker_ip, broker_port);
-// }
 
 void MQTTController::poll()
 {
@@ -41,6 +35,6 @@ void MQTTController::poll()
 void MQTTController::send(String msg)
 {
     _mqtt.beginMessage(topic);
-    _mqtt.print(msg);
+    _mqtt.println(msg);
     _mqtt.endMessage();
 }
