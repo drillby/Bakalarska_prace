d195d7e851841ad9efeac5b52b97891fde80b4ba
drillby <podrazkypavel002@gmail.com>
2023-06-14 19:30:03 +0200

    moved config

--- api/__init__.py
+++ api/__init__.py
@@ -6,31 +6,13 @@ from flask_mqtt import Mqtt
 
 # creating the flask app
 app = Flask(__name__, instance_relative_config=True)
-app.config["SECRET"] = "secret!123"
-
-app.config["MQTT_BROKER_URL"] = "test.mosquitto.org"
-app.config["MQTT_BROKER_PORT"] = 8883
-# app.config[
-#     "MQTT_USERNAME"
-# ] = ""  # Set this item when you need to verify username and password
-# app.config[
-#     "MQTT_PASSWORD"
-# ] = ""  # Set this item when you need to verify username and password
-app.config["MQTT_KEEPALIVE"] = 5  # Set KeepAlive time in seconds
-app.config["MQTT_TLS_ENABLED"] = True  # If your server supports TLS, set it True
-app.config["MQTT_TLS_CA_CERTS"] = "./cert/mosquitto.org.pem"
-app.config["MQTT_TLS_CERTFILE"] = "./cert/certificate.pem"
-app.config["MQTT_TLS_KEYFILE"] = "./cert/private.key"
-app.config["MQTT_TLS_CIPHERS"] = "ECDHE-RSA-AES128-GCM-SHA256"
-app.config["MQTT_TLS_VERSION"] = ssl.PROTOCOL_TLSv1_2
-
-
-topic = "test"
-
-mqtt_reciever = Mqtt(app, mqtt_logging=True)
 
 # loading configuraiton
-app.config.from_object("config.Config")
+app.config.from_object("config.MQTTConfig")
+
+# enable MQTT 
+mqtt_reciever = Mqtt(app, mqtt_logging=True)
+
 
 # enabling CORS
 API_CORS_CONFIG = {
--- config.py
+++ config.py
@@ -1,2 +1,18 @@
-class Config:
+import ssl
+
+
+class MQTTConfig:
+    MQTT_BROKER_URL = "test.mosquitto.org"
+    MQTT_BROKER_PORT = 8883
+    MQTT_KEEPALIVE = 5  # Set KeepAlive time in seconds
+    MQTT_TLS_ENABLED = True  # If your server supports TLS, set it True
+    MQTT_TLS_CA_CERTS = "./cert/mosquitto.org.pem"
+    MQTT_TLS_CERTFILE = "./cert/certificate.pem"
+    MQTT_TLS_KEYFILE = "./cert/private.key"
+    MQTT_TLS_CIPHERS = "ECDHE-RSA-AES128-GCM-SHA256"
+    MQTT_TLS_VERSION = ssl.PROTOCOL_TLSv1_2
+    # MQTT_TOPIC = "bp/arduino/proximity_sensor"
+    MQTT_TOPIC = "test"
+
+class DatabaseConfig:
     ...
