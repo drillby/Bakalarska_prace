106a4a8178f19985b744df2bfd3c80e5e5bab4c6
drillby <podrazkypavel002@gmail.com>
2023-06-19 20:01:52 +0200

    created get data view

--- app/endpoints/frondend.py
+++ app/endpoints/frondend.py
@@ -1,9 +1,32 @@
 from flask import jsonify, request
 
-from app import app
+from app import api_logger, app, db
+from app.models.Passing import Passing
 
 
-@app.route("/write_db", methods=["POST"])
-def write_to_db():
-    print(request.json)
-    return jsonify({"status": "OK"})
+@app.route("/get_data", methods=["GET"])
+def get_from_db():
+    from_datatime = request.args.get("from_datetime")
+    to_datetime = request.args.get("to_datetime")
+    on_datetime = request.args.get("on_datetime")
+    is_red = request.args.get("is_red")
+
+    query = db.session.query(Passing)
+    if from_datatime:
+        query = query.filter(Passing.datetime >= from_datatime)
+    if to_datetime:
+        query = query.filter(Passing.datetime <= to_datetime)
+    if on_datetime and (not from_datatime or not to_datetime):
+        query = query.filter(Passing.datetime == on_datetime)
+    if is_red:
+        query = query.filter(Passing.is_red == is_red)
+
+    query = query.all()
+
+    api_logger.info(f"Recieved query: {query}")
+
+    return jsonify(
+        {
+            "data": [passing.to_dict() for passing in query],
+        }
+    )
