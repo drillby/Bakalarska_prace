00429d2c607989afde151cc3d48513ab7b27cc80
drillby <podrazkypavel002@gmail.com>
2023-06-19 20:30:24 +0200

    finalized get_data endpoint

--- app/endpoints/frondend.py
+++ app/endpoints/frondend.py
@@ -1,4 +1,9 @@
-from flask import jsonify, request
+import datetime
+import tempfile
+from typing import List
+
+import pandas as pd
+from flask import jsonify, request, send_file
 
 from app import api_logger, app, db
 from app.models.Passing import Passing
@@ -6,24 +11,34 @@ from app.models.Passing import Passing
 
 @app.route("/get_data", methods=["GET"])
 def get_from_db():
-    from_datatime = request.args.get("from_datetime")
+    from_datetime = request.args.get("from_datetime")
     to_datetime = request.args.get("to_datetime")
     on_datetime = request.args.get("on_datetime")
     is_red = request.args.get("is_red")
 
-    query = db.session.query(Passing)
-    if from_datatime:
-        query = query.filter(Passing.datetime >= from_datatime)
+    if from_datetime:
+        from_datetime = datetime.datetime.strptime(from_datetime, "%Y-%m-%d %H:%M:%S")
     if to_datetime:
-        query = query.filter(Passing.datetime <= to_datetime)
-    if on_datetime and (not from_datatime or not to_datetime):
-        query = query.filter(Passing.datetime == on_datetime)
+        to_datetime = datetime.datetime.strptime(to_datetime, "%Y-%m-%d %H:%M:%S")
+    if on_datetime:
+        on_datetime = datetime.datetime.strptime(on_datetime, "%Y-%m-%d %H:%M:%S")
+    if is_red:
+        is_red = True if is_red == "True" else False
+
+    query = db.session.query(Passing)
+    if from_datetime:
+        query = query.filter(Passing.date_time >= from_datetime)
+    if to_datetime:
+        query = query.filter(Passing.date_time <= to_datetime)
+    if on_datetime and (not from_datetime or not to_datetime):
+        query = query.filter(Passing.date_time == on_datetime)
     if is_red:
         query = query.filter(Passing.is_red == is_red)
 
     query = query.all()
 
     api_logger.info(f"Recieved query: {query}")
+    query: List[Passing] = query.all()
 
     return jsonify(
         {
