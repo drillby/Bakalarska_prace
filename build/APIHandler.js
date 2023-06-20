a219b1450ae4810f22e991190d5a0ed4c2b4fdfd
drillby <podrazkypavel002@gmail.com>
2023-06-20 22:21:14 +0200

    builded

--- build/APIHandler.js
+++ build/APIHandler.js
@@ -9,31 +9,22 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// create APIHandler class
-class APIHandler {
-    // create constructor
-    constructor(url, port) {
-        // create base url
-        this.baseURL = url;
-        this.port = port;
-    }
-    // create method for getting table entries by given parameters: from_datetime, to_datetime, on_datetime, is_red
-    // typical request looks like this http://192.168.132.103:8000/get_data?from_datetime=2021-01-01%2000:00:00
-    getTableEntries(from_datetime, to_datetime, on_datetime, is_red) {
-        return __awaiter(this, void 0, void 0, function* () {
-            // create URLSearchParams object
-            const params = new URLSearchParams();
-            // add params to URLSearchParams object
-            params.append("from_datetime", from_datetime.toString());
-            params.append("to_datetime", to_datetime.toString());
-            params.append("on_datetime", on_datetime.toString());
-            params.append("is_red", is_red.toString());
-            // create response object
-            const response = yield fetch(`${this.baseURL}:${this.port}/get_data?${params}`);
-            // create json object
-            const json = yield response.json();
-            // return json
-            return json;
-        });
-    }
+// create method for getting table entries by given parameters: from_datetime, to_datetime, on_datetime, is_red
+// typical request looks like this http://192.168.132.103:8000/get_data?from_datetime=2021-01-01%2000:00:00
+function getTableEntries(formVals, serverConf) {
+    return __awaiter(this, void 0, void 0, function* () {
+        // create URLSearchParams object
+        const params = new URLSearchParams();
+        // add params to URLSearchParams object
+        params.append("from_datetime", formVals.from_date.toString());
+        params.append("to_datetime", formVals.to_date.toString());
+        params.append("on_datetime", formVals.on_date.toString());
+        params.append("is_red", formVals.is_red.toString());
+        // create response object
+        const response = yield fetch(`${serverConf.url}:${serverConf.port}/get_data?${params}`);
+        // create json object
+        const json = yield response.json();
+        // return json
+        return json;
+    });
 }
