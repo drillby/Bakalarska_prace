b83bcb414389383de240882af24ab7965a2b0211
drillby <podrazkypavel002@gmail.com>
2023-06-24 11:12:15 +0200

    updated IP

--- config/APIConfig.h
+++ config/APIConfig.h
@@ -6,7 +6,7 @@
 // adresa testovacího serveru na lokální síti
 #define LOCAL_SERVER       \
     {                      \
-        192, 168, 132, 103 \
+        192, 168, 132, 156 \
     }
 // port serveru
 #define SERVER_PORT 8000
