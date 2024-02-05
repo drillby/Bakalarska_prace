3f36ea27cb2ffdb073e32631e63ba6e4d519f232
Pavel Podrazký <podrazkypavel002@gmail.com>
2024-02-05 14:33:22 +0100

    Update API server address and port

--- config/APIConfig.h
+++ config/APIConfig.h
@@ -2,13 +2,13 @@
 #define APIConfig_h
 
 // URL adresa API serveru
-#define SERVER_ADDRESS "bakalarska-prace.azurewebsites.net"
+#define SERVER_ADDRESS "bp_api.hostgo.cloud"
 // adresa testovacího serveru na lokální síti
 #define LOCAL_SERVER       \
     {                      \
         192, 168, 132, 156 \
     }
 // port serveru
-#define SERVER_PORT 8000
+#define SERVER_PORT 80
 
 #endif
